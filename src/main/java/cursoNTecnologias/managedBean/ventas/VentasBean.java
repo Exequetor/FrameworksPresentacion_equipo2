a6c2744b642dcb6c5f676f81ab294ebdacea9076
Carlos Montellano <exequetor@gmail.com>
2019-06-11 07:50:31 -0500

    Agregado read/update de ventas

--- src/main/java/cursoNTecnologias/managedBean/ventas/VentasBean.java
+++ src/main/java/cursoNTecnologias/managedBean/ventas/VentasBean.java
@@ -4,61 +4,60 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.primefaces.event.CellEditEvent;
 import org.primefaces.event.RowEditEvent;
 
-import cursoNTecnologias.bd.domain.Productos;
+
 import cursoNTecnologias.bd.domain.Ventas;
-import cursoNTecnologias.service.ganancias.GananciasService;
 import cursoNTecnologias.service.ventas.VentasService;
 
-@ManagedBean
+@Named
 @ViewScoped
-public class VentasBean implements Serializable{
-	
-	private static final long serialVersionUID = -7291372737200601998L;
+public class VentasBean implements Serializable {
+	private static final long serialVersionUID = 6705556644665739772L;
 	
 	@Inject
 	VentasService service;
-	
 	List<Ventas> ventasList = null;
 	
 	public void setVentasList(List<Ventas> ventasList) {
 		this.ventasList = ventasList;
 	}
-
 	public List<Ventas> getVentasList() {
 		if (ventasList == null)
 			setVentasList(service.queryAllVentas());
 		return ventasList;
 	}
-
 	public void onRowEdit(RowEditEvent event) {
+		System.out.println("On row edit");
 		Ventas venta = ((Ventas) event.getObject());
-		System.out.println("Datos ventas: " + venta);
-		
-		FacesMessage msg = new FacesMessage("Productos editado", Productos.getIdproducto().toString());
+		System.out.println("Datos ventas: \n" + venta);
+		service.updateVenta(venta);
+		FacesMessage msg = new FacesMessage("Venta editada", venta.getIdventa().toString());
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 
 	public void onRowCancel(RowEditEvent event) {
-		FacesMessage msg = new FacesMessage("Edicion cancelada", ((Productos) event.getObject()).getIdproducto().toString());
+		System.out.println("On row cancel");
+		FacesMessage msg = new FacesMessage("Edicion cancelada", ((Ventas) event.getObject()).getIdventa().toString());
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 
 	public void onCellEdit(CellEditEvent event) {
+		System.out.println("On cell edit");
 		Object oldValue = event.getOldValue();
 		Object newValue = event.getNewValue();
 		System.out.println("verifica: " + newValue);
 		if (newValue != null && !newValue.equals(oldValue)) {
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Productos modificado",
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Venta modificada",
 					"Antes: " + oldValue + ", Ahora: " + newValue);
 			FacesContext.getCurrentInstance().addMessage(null, msg);
 		}
 	}
+	
 }
